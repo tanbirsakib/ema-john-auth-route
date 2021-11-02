17a4cbf399c08b6dda59970cbffbcf81e6172bf9
tanbirsakib <iamtsakib@gmail.com>
2021-11-02 13:25:23 +0600

    auth route

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC22y8dVN9gl8XDsLRxtRfVoCEer3PZJHw",
+    authDomain: "ema-john-auth-b5e6a.firebaseapp.com",
+    projectId: "ema-john-auth-b5e6a",
+    storageBucket: "ema-john-auth-b5e6a.appspot.com",
+    messagingSenderId: "149078215424",
+    appId: "1:149078215424:web:201a50af978ffa70ac81e5"
+  };
+
+  export default firebaseConfig;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+
+const initFirebaseAuth = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default initFirebaseAuth;
